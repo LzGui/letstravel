e116484e2b348d4dc2245a563a954c0e7a8c3691
LzGui <lzguilhermecp@gmail.com>
2021-11-01 15:44:42 -0300

    atualizando

---
+++ src/main/java/com/letstravel/pagamento/security/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.letstravel.pagamento.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
